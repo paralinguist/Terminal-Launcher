d80c265cf4da3ca103e325c213a3b4ba48ec95bc
Josh <72240549+potaoTT@users.noreply.github.com>
2023-11-24 10:50:30 +0800

    Some visual changes and starting to work on the change of launch button

--- intro.gd
+++ intro.gd
@@ -8,8 +8,8 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    yield(get_tree().create_timer(10.3), "timeout")
-    get_tree().change_scene("res://Scenes/Main/Main.tscn")
+	yield(get_tree().create_timer(10.3), "timeout")
+	get_tree().change_scene("res://Scenes/Main/Main.tscn")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
